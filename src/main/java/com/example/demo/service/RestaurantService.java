09c9a4bbd80c04003dadc4a6f1be36fbd2a2e124
sayarya <64267777+sayarya@users.noreply.github.com>
2021-07-28 17:59:21 +0530

    Create RestaurantService.java

---
+++ src/main/java/com/example/demo/service/RestaurantService.java
@@ -0,0 +1,40 @@
+package com.example.demo.service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.repos.RestaurantRepository;
+import com.example.demo.entity.*;
+@Service
+public class RestaurantService {
+
+	
+	@Autowired
+	private RestaurantRepository repo;
+	
+	
+	public List<Restaurant> findAll(){
+		
+		return this.repo.findAll();
+	}
+	
+	
+	public Restaurant add(Restaurant entity) {
+		
+		return this.repo.save(entity);
+	}
+public Restaurant delete(Restaurant entity) {
+		
+		repo.delete(entity);
+		return entity;
+	}
+
+public Optional<Restaurant> findById(int id) {
+	
+	return this.repo.findById(id);
+}
+}
