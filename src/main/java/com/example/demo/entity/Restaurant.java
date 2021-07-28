7efa11a5d9306924b7a908d653705833f92715f8
sayarya <64267777+sayarya@users.noreply.github.com>
2021-07-28 17:59:53 +0530

    Create Restaurant.java

---
+++ src/main/java/com/example/demo/entity/Restaurant.java
@@ -0,0 +1,32 @@
+package com.example.demo.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldDefaults;
+
+@Entity
+@Table(name="india_hotels")
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Restaurant {
+	
+	@Id
+	int id;
+	String restaurantName;
+	String location;
+	String category;
+	double rating;
+	
+	
+	
+
+}
